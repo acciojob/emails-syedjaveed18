2956ef072a1f0575df0f0d948b85a6670c2fe90e
Syed Javeed <syedjm18@gmail.com>
2023-02-18 20:45:45 +0530

    third commit

--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -29,16 +29,41 @@ public class Workspace extends Gmail{
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
         // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
 
-        int count = 1;
-        Meeting currentMeeting = calendar.get(0);
-        LocalTime endTime = currentMeeting.getEndTime();
-        for(int i = 1; i < calendar.size(); i++){
-            Meeting nextMeeting = calendar.get(i);
-            if(nextMeeting.getEndTime().compareTo(endTime) > 0){
-                count++;
-                endTime = nextMeeting.getEndTime();
+//        int count = 1;
+//        Meeting currentMeeting = calendar.get(0);
+//        LocalTime endTime = currentMeeting.getEndTime();
+//        for(int i = 1; i < calendar.size(); i++){
+//            Meeting nextMeeting = calendar.get(i);
+//            if(nextMeeting.getEndTime().compareTo(endTime) > 0){
+//                count++;
+//                endTime = nextMeeting.getEndTime();
+//            }
+//        }
+//        return count;
+
+        ArrayList<Pair<LocalTime, Integer>> endTimes = new ArrayList<>();
+
+        for (int i = 0; i < calendar.size(); i++) {
+            endTimes.add(Pair.of(calendar.get(i).getEndTime(), i));
+        }
+
+        Collections.sort(endTimes);
+
+        LocalTime time_limit = endTimes.get(0).getLeft();
+
+        int cnt = 0;
+        if(!endTimes.isEmpty()) {
+            cnt += 1;
+        }
+
+        for (int i = 1; i < endTimes.size(); i++) {
+            if (calendar.get(endTimes.get(i).getRight()).getStartTime().compareTo(time_limit) > 0) {
+
+                cnt += 1;
+                time_limit = endTimes.get(i).getLeft();
             }
         }
-        return count;
+
+        return cnt;
     }
 }
